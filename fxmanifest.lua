8c769d93dec9725f77f554b0f05592b88c2b664a
cyneptic <50639733+cyneptic@users.noreply.github.com>
2023-03-18 17:19:13 +0100

    moved shared scripts above server scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,11 @@ fx_version 'bodacious'
 game 'gta5'
 lua54 'yes'
 
+shared_script 'config.lua'
+
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'server/main.lua'
 }
 
 client_script 'client/main.lua'
-shared_script 'config.lua'
